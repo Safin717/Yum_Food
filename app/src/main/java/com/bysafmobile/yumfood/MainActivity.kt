a6a84f33284f2a4301338d67bc2466579c042f47
Azat <saa20@inbox.ru>
2022-12-06 15:46:25 +0300

    added navigation component with 3 fragments, bottomnavigationView and drawable vector assets

--- app/src/main/java/com/bysafmobile/yumfood/MainActivity.kt
+++ app/src/main/java/com/bysafmobile/yumfood/MainActivity.kt
@@ -2,10 +2,19 @@ package com.bysafmobile.yumfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        // получаем navController в Activity:
+        val navController = Navigation.findNavController(this, R.id.hots_fragment)
+        // задаем bottomNavigation для использования с NavController
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
