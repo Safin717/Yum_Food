3e699823287f630ac8cd5548ae9fc6a0db3bfeba
Azat <saa20@inbox.ru>
2022-12-07 13:35:46 +0300

    made the second MealActivity and markup xml file

---
+++ app/src/main/java/com/bysafmobile/yumfood/activities/MealActivity.kt
@@ -0,0 +1,12 @@
+package com.bysafmobile.yumfood.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bysafmobile.yumfood.R
+
+class MealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+    }
+}
--- app/src/main/java/com/bysafmobile/yumfood/fragments/HomeFragment.kt
+++ app/src/main/java/com/bysafmobile/yumfood/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.bysafmobile.yumfood.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.bysafmobile.yumfood.activities.MealActivity
 import com.bysafmobile.yumfood.data.Meal
 import com.bysafmobile.yumfood.databinding.FragmentHomeBinding
 import com.bysafmobile.yumfood.viewmodel.HomeViewModel
@@ -34,7 +36,15 @@ class HomeFragment : Fragment() {
 
         homeMvvm.getRandomMeal()
         observerRandomMeal()
+        onRandomMealClick()
 
+    }
+
+    private fun onRandomMealClick() {
+        binding.randomMealCard.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun observerRandomMeal() {
