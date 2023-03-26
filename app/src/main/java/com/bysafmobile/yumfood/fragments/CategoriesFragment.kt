eaf9cd1e879133ce26cd71aa5c8ac33d5357447b
Azat <saa20@inbox.ru>
2023-03-26 16:36:32 +0300

    fixed application crash in category fragment

--- app/src/main/java/com/bysafmobile/yumfood/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/bysafmobile/yumfood/fragments/CategoriesFragment.kt
@@ -1,6 +1,8 @@
 package com.bysafmobile.yumfood.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bysafmobile.yumfood.R
+import com.bysafmobile.yumfood.activities.CategoryMealsActivity
 import com.bysafmobile.yumfood.activities.MainActivity
 import com.bysafmobile.yumfood.adapters.CategoriesAdapter
 import com.bysafmobile.yumfood.databinding.FragmentCategoriesBinding
@@ -40,6 +43,8 @@ class CategoriesFragment : Fragment() {
         prepareRecyclerView()
 
         observeCategories()
+
+        onCategoryClick()
     }
 
     private fun observeCategories() {
@@ -53,6 +58,14 @@ class CategoriesFragment : Fragment() {
         binding.rvCategoris.apply {
             layoutManager = GridLayoutManager(context, 3, GridLayoutManager.VERTICAL, false)
             adapter = categoriesAdapter
+        }
+    }
+    private fun onCategoryClick() {
+        categoriesAdapter.onItemClick = {category ->
+            val intent = Intent(activity, CategoryMealsActivity::class.java)
+            intent.putExtra(HomeFragment.CATEGORY_NAME, category.strCategory)
+            startActivity(intent)
+            Log.d("Category", "Category")
         }
     }
 }
